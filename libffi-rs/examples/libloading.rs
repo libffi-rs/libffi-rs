61d6a4d939389ca4f05c14c458a8272510a577ea
Ashwin Naren <arihant2math@gmail.com>
2025-09-29 23:38:33 -0700

    add libloading example to show how dynamic calling works

--- libffi-rs/examples/libloading.rs
+++ libffi-rs/examples/libloading.rs
@@ -2,9 +2,9 @@
 //! from a shared library at runtime. It loads the C standard library and calls the
 //! `isdigit` function of libc to check if a character is a digit.
 
-use std::ffi::c_void;
 use libffi::high::CodePtr;
 use libffi::middle::{arg, Cif, Type};
+use std::ffi::c_void;
 
 #[cfg(target_os = "macos")]
 const FILE: &str = "/usr/lib/libSystem.B.dylib";
@@ -13,14 +13,17 @@ const FILE: &str = "/lib/x86_64-linux-gnu/libc.so.6";
 #[cfg(target_os = "windows")]
 const FILE: &str = r"C:\Windows\System32\msvcrt.dll";
 
+#[cfg(not(any(target_os = "macos", target_os = "linux", target_os = "windows")))]
+fn main() {
+    println!("This example is only supported on macOS, Linux, and Windows.");
+}
+
+#[cfg(any(target_os = "macos", target_os = "linux", target_os = "windows"))]
 fn main() {
     // Parameters - Feel free to change these to call other functions
     // from the C standard library!
     let func_name = "isdigit";
-    let cif = Cif::new(
-        vec![Type::i32()],
-        Type::i32(),
-    );
+    let cif = Cif::new(vec![Type::i32()], Type::i32());
     let args1 = [arg(&('1' as i32))];
     let args2 = [arg(&('a' as i32))];
     // None of what we're doing here is safe
@@ -35,17 +38,9 @@ fn main() {
         let func = func.into_raw().as_raw_ptr();
 
         // Now we proceed as normal with libffi-rs
-        let result: i32 =
-            cif.call(
-                CodePtr(func as *mut _),
-                &args1
-            );
+        let result: i32 = cif.call(CodePtr(func as *mut _), &args1);
         assert_eq!(result, 1);
-        let result: i32 =
-            cif.call(
-                CodePtr(func as *mut _),
-                &args2
-            );
+        let result: i32 = cif.call(CodePtr(func as *mut _), &args2);
         assert_eq!(result, 0);
     }
 }
